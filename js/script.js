a811763cf201821f1da60cb364b25a5a6f4aaed0
Kateryna Navozenko <katrinelle19@gmail.com>
2025-02-13 00:24:31 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,16 +1,67 @@
-const btn = document.querySelector(".js-get-data");
+// -----
+
+// title = "CRUD";
+// crud_post = Post.objects.get((title = title));
+
+// Post.object.create((title = "New post"), (text = "Next of new post"));
+// post = Post.object.get((title = "New post"));
+// print(post.title);
+
+// -----
+
+// async function f() {
+//   return 1;
+// }
+// f().then(alert); // 1 у вспливаючому вікні
+
+// те ж саме:
+
+// async function f() {
+//   return Promise.resolve(1);
+// }
+// f().then(alert);
+
+// -----
+
+// async function f() {
+//   await Promise.reject(new Error("Ups!!"));
+// }
+
+// // робить те ж саме, що и цей
+
+// async function f() {
+//   throw new Error("Ups!!");
+// }
+
+// -----
+
+// LESSON 13
+
+// -----
+
+// const btn = document.querySelector(".js-get-data");
 
 // btn.addEventListener("click", getUrl);
 
 // function getUrl() {
-//   // return fetch("https://api.monobank.ua/bank/currency");
+//   //   return fetch("https://api.monobank.ua/bank/currency"); // запит на курс валют від монобанку
+// разшифровка коду: сеть/нажимаємо на currency/пред перегляд/копіюємо/вставляємо в
+// http://json.parser.online.fr/
+
 //   return fetch("https://the-cocktail-db.p.rapidapi.com/search.php'");
-//   //   fetch(
-//   //     "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits"
-//   //   );
+//   fetch(
+//     "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits"
+//   );
 // }
 
+// всі api отримуємо з інету
+// наприклад: курс валют/ пошук api.monobank/копіюємо посилання
+
+// -----
+// fetch
+
 // let promise = fetch(url, {
+//   // Опції
 //   method: "GET", // POST, PUT, DELETE та інші
 //   headers: {
 //     // значення цього заголовку зазвичай встановлюється автоматично,
@@ -31,15 +82,21 @@ const btn = document.querySelector(".js-get-data");
 //   window: window, // null
 // });
 
+// -----
 // async/await
 
 // async function a() {
 //   return true;
 // }
 
-// a().then(console.log);
+// a().then(console.log); // true
+
+// -----
+// await - призупиняє віконання функції, поки promise не виконається
+// не можна використовувати await у звичайних функціяї (неасинхронних)
 
 // let value = await promise
+
 // async function a() {
 //   let prom = new Promise((resolve, reject) => {
 //     setTimeout(() => resolve("Hello"), 1000);
@@ -50,116 +107,172 @@ const btn = document.querySelector(".js-get-data");
 //   console.log(result);
 // }
 
-// a();
+// a(); // Hello через секудну
 
-// function b (){
+// так не можна:
+
+// function b() {
 //     let result = await promise
 // }
 
-// return fetch("https://api.monobank.ua/bank/currency");
+// але можуть працювати в верхніх модулях
+
+// let response = await fetch("https://api.monobank.ua/bank/currency");
+
+// -----
+// fetch
 
 // const monocurrency = async () => {
-//   let response = await fetch(
-//     "https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits"
-//   );
+//   let response = await fetch("https://api.monobank.ua/bank/currency");
 
 //   if (response.ok) {
 //     console.log("Ok");
 //     console.log("response", response.json());
 //   } else {
-//     console.log("Err: ", response.stteatus);
+//     r: console.log("Err: ", response.status);
 //   }
 // };
 
-// monocurrency();
+// monocurrency(); // Ok
+
+// -----
+// headers
 
 // const headers = new Headers({
 //   "Content-Type": "application/json",
 //   "X-Custom-Header": "custom value",
 // });
 
-// headers.has("Content-Type")
+// headers.has("Content-type");
 
 // const postTest = async () => {
 //   let user = {
 //     name: "Ivan",
-//     city: "Kyiv",
+//     city: "Lviv",
 //   };
 
+//   // відправляємо ці дані в post запит
 //   let response = await fetch(url, {
-//     method: "POST",
+//     metod: "POST",
 //     headers: {
 //       "Content-Type": "application/json",
 //     },
 //   });
 // };
 
+// postTest().then();
+
+// -----
+// try catch - робота з помилками
+// працює синхронно
+
 // try {
 //   console.log("Start");
-//   a;
+//   //   a;
 //   console.log("Finish");
 // } catch (err) {
-//   console.log("Errr");
+//   console.log("Error");
 // }
 
 // try {
 //   setTimeout(function () {
-//     da;
+//     console.log("d");
 //   }, 1000);
 // } catch (err) {
-//   console.log("Errr");
+//   console.log("Errrr");
 // }
 
 // setTimeout(function () {
 //   try {
-//     da;
+//     console.log("Ok");
 //   } catch (err) {
-//     console.log("Errr");
+//     console.log("Errrrr");
 //   }
 // }, 1000);
+
+// виводимо помилки
+
+// try {
+//   da;
+// } catch (err) {
+//   console.log(err.name);
+// } // ReferenceError
+
+// try {
+//   da;
+// } catch (err) {
+//   console.log(err.message);
+// } // da is not defined
 
 // try {
 //   da;
 // } catch (err) {
 //   console.log(err.stack);
-// }
+// } // ReferenceError: da is not defined
+
+// -----
+
+// let json = ``;
+
+// try {
+//   let user = JSON.parse(json);
+// } catch (err) {
+//   console.log("Errrrr", err.message);
+// } // Errrrr Unexpected end of JSON input якщо прийшов не json
+
+// -----
+// finally - виконується завжди
 
 // let json = `{"name": "Ann"}`;
 
 // try {
 //   let user = JSON.parse(json);
-//   // if (!user.city) {
-//   //   throw new SyntaxError("Нема міста");
-//   // }
-//   console.log("start");
+//   console.log("user", user); // user name: Ann
+//   console.log("city", user.city); // city undefined
+//   if (!user.city) {
+//     throw new SyntaxError("Немає міста");
+//   } // Errrr немає міста
 // } catch (err) {
-//   console.log("Err", err.message);
+//   console.log("Errrrr", err.message);
 // } finally {
-//   console.log("finally");
+//   console.log("finally"); // finally
 // }
+
+// -----
+// web socket
+
+// встановлюємо web socet зєднання
 
 // let socket = new WebSocket(
 //   "wss://javascript.info/article/websocket/demo/hello"
 // );
 
+// // -----
+// // події:
+
+// // onopen
 // socket.onopen = function (e) {
-//   console.log("З*єднання встановленно");
-//   socket.send("Привіт");
-//   socket.send("Я Христя");
+//   console.log("Connection ok"); // Connection ok
+//   socket.send("GoodDay");
 // };
 
+// // onmessage
 // socket.onmessage = function (e) {
 //   console.log("message", e.data);
-// };
+// }; // message Hello from server, GoodDay!
 
-// socket.onclose = function (event) {
-//   if (event.wasClean) {
-//     console.log("Зупинено", event.code);
-//   } else {
-//     console.log("Poзірвано");
-//   }
-// };
-
+// // onerror
 // socket.onerror = function (err) {
 //   console.log(err);
 // };
+
+// // onclose
+// socket.onclose = function (event) {
+//   if (event.wasClean) {
+//     console.log("StopConnection", event.code);
+//   } else {
+//     console.log("Disconnect");
+//   }
+// }; // StopConnection 1000
+
+// // -----
